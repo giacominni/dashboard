765bac3e425d312852e8e320b9ba05c2f00146fc
giacominni <giovannigiacomini13@gmail.com>
2026-02-26 19:40:27 -0300

    Add Clientes and Produtos routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Sidebar     from './components/Sidebar'
 import Dashboard   from './pages/Dashboard/Dashboard'
 import Faturamento from './pages/Faturamento/Faturamento'
+import Clientes    from './pages/Clientes/Clientes'
+import Produtos    from './pages/Produtos/Produtos'
 import './styles/global.css'
 import styles from './App.module.css'
 
@@ -15,6 +17,8 @@ export default function App() {
             <Route path="/"            element={<Navigate to="/dashboard" replace />} />
             <Route path="/dashboard"   element={<Dashboard />} />
             <Route path="/faturamento" element={<Faturamento />} />
+            <Route path="/clientes"    element={<Clientes />} />
+            <Route path="/produtos"    element={<Produtos />} />
           </Routes>
         </main>
       </div>
